9f1ee7df81e573d55a383816685c1abd92247093
jingGM <liangjingjerry@gmail.com>
2019-07-21 19:27:26 -0400

    update Interface with velocity control

--- navisim/scripts/Interface.py
+++ navisim/scripts/Interface.py
@@ -13,6 +13,7 @@ import message_filters
 
 from sensor_msgs.msg import LaserScan, CameraInfo, Image
 from rosgraph_msgs.msg import Clock
+from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
 
 
@@ -20,18 +21,18 @@ def resetenvironment():
     rospy.wait_for_service('/gazebo/reset_world')
     reset_simulation = rospy.ServiceProxy('/gazebo/reset_world', Empty)
     reset_simulation()
+    #d = rospy.Duration(10, 0)
+    #rospy.sleep(d)
 
+def talker(cmd,i):
 
-def talker(cmd):
-    pub = rospy.Publisher('test_hahahaha', String, queue_size=10)
+    #for i in range(RobotNumber):
+    topicname = '/jackal%d/jackal_velocity_controller/cmd_vel'%i
+    pub = rospy.Publisher(topicname, Twist, queue_size=10)
     
-    rate = rospy.Rate(10) # 10hz
-    
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+    pub.publish(cmd)
+    #rate = rospy.Rate(10) # 10hz
+    #rate.sleep()
 
 def listener(RobotNumber,SubData):
 
@@ -63,6 +64,20 @@ def callback(scan, cam, dcam,i):
     print(cam.header)
     print(i)
 
+    vel = Twist()
+    vel.linear.x = 5
+    talker(vel,i)
+
+    global TimeStart
+    global TimeElapsed 
+    if TimeElapsed<5:
+        TimeElapsed = rospy.get_time() - TimeStart
+        print(TimeElapsed)
+    else:
+        resetenvironment()
+        print("reseted")
+        TimeStart = rospy.get_time()
+        TimeElapsed=0
 
 def cleardata(SubData):
     SubData = {'clock':[], 'scan':[], 'cam':[], 'dcam':[]}
@@ -70,13 +85,13 @@ def cleardata(SubData):
 if __name__ == '__main__':
     rospy.init_node('test', anonymous=True)
 
+    TimeStart = rospy.get_time()
+    TimeElapsed = 0
+
     RobotNumber=4
 
     SubData = {'clock':[], 'scan':[], 'cam':[], 'dcam':[]}
 
     listener(RobotNumber,SubData)
 
-    #try:
-        #talker()
-    #except rospy.ROSInterruptException:
-    #    pass
+    
